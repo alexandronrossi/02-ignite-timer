1654cbb6dc7b1ed550e2221a2f37a515f5fcec46
Alexandro do Nascimento Rossi <alexandronrossi@hotmail.com>
2024-08-04 20:04:38 -0300

    Aula: Cores e fontes

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,9 +1,19 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
 
 interface ButtonContainerProps {
-	// variant: ButtonVariant;
+	variant: ButtonVariant;
+}
+
+const buttonVariant = {
+	primary: 'purple',
+	secondary: 'purple',
+	danger: 'purple',
+	success: 'purple'
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-	/* background-color: ${props.theme.white}; */
+	background-color: ${props => props.theme["gray-500"]};
+	color: ${props => props.theme.white};
 `
