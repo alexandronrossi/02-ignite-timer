f0ea16d948c1067a55d1e6a64f4af99a4396b63c
Alexandro do Nascimento Rossi <alexandronrossi@hotmail.com>
2024-08-04 19:52:02 -0300

    Aula: Estilos globais

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+interface ButtonContainerProps {
+	// variant: ButtonVariant;
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+	/* background-color: ${props.theme.white}; */
+`
