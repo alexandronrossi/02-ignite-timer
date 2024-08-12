50b67f00844a8a58cd158a060ae22ac0a6338ba9
Alexandro do Nascimento Rossi <alexandronrossi@hotmail.com>
2024-08-12 18:23:18 -0300

    Aula: Iniciando um novo ciclo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useState } from 'react'
 import * as zod from 'zod'
 
 import {
@@ -23,7 +24,16 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+	id: string
+	task: string
+	minutesAmount: number
+}
+
 export function Home() {
+	const [cycles, setCycles] = useState<Cycle[]>([])
+	const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
 	const { register, handleSubmit, watch, formState, reset } =
 		useForm<NewCycleFormData>({
 			resolver: zodResolver(newCycleFormValidationSchema),
@@ -34,11 +44,21 @@ export function Home() {
 		})
 
 	function handleCreateNewCycle(data: NewCycleFormData) {
-		console.log(data)
+		const id = String(new Date().getTime())
+
+		const newCycle: Cycle = {
+			id,
+			task: data.task,
+			minutesAmount: data.minutesAmount,
+		}
+
+		setCycles((state) => [...state, newCycle])
+		setActiveCycleId(id)
+
 		reset()
 	}
 
-	console.log(formState.errors)
+	const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
 	const task = watch('task')
 	const isSubmitDisabled = !task
