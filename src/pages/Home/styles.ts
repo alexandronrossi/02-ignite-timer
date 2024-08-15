bdafd26d0f5080d6fcc4a27411a329ce0963850f
Alexandro do Nascimento Rossi <alexandronrossi@hotmail.com>
2024-08-15 00:03:01 -0300

    Curso: Criando SPAs com ReactJS | Módulo: Funcionalidades da aplicação | Aula: Interromper ciclo

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -94,7 +94,7 @@ export const Separator = styled.div`
 	justify-content: center;
 `
 
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
 	display: flex;
 	align-items: center;
 	justify-content: center;
@@ -109,15 +109,26 @@ export const StartCountdownButton = styled.button`
 
 	cursor: pointer;
 
-	background: ${(props) => props.theme['green-500']};
 	color: ${(props) => props.theme['gray-100']};
 
 	&:disabled {
 		opacity: 0.7;
 		cursor: not-allowed;
 	}
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+	background: ${(props) => props.theme['green-500']};
 
 	&:not(:disabled):hover {
 		background: ${(props) => props.theme['green-700']};
+	}
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+	background: ${(props) => props.theme['red-500']};
+
+	&:not(:disabled):hover {
+		background: ${(props) => props.theme['red-700']};
 	}
 `
