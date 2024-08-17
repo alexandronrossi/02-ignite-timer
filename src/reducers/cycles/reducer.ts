0e55f00e1d19c1c7dc7b335c249acefd8e0ec212
Alexandro do Nascimento Rossi <alexandronrossi@hotmail.com>
2024-08-17 19:30:45 -0300

    Curso: Criando SPAs com ReactJS | Módulo: Reducers | Aula: Utilizando immer

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,3 +1,4 @@
+import { produce } from 'immer'
 import { ActionTypes } from './actions'
 
 export interface Cycle {
@@ -17,32 +18,40 @@ interface CyclesState {
 export function cyclesReducer(state: CyclesState, action: any) {
 	switch (action.type) {
 		case ActionTypes.ADD_NEW_CYCLE:
-			return {
-				...state,
-				cycles: [...state.cycles, action.payload.newCycle],
-				activeCycleId: action.payload.newCycle.id,
+			return produce(state, (draft) => {
+				draft.cycles.push(action.payload.newCycle)
+				draft.activeCycleId = action.payload.newCycle.id
+			})
+
+		case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
+			const currentCycleIndex = state.cycles.findIndex((cycle) => {
+				return cycle.id === state.activeCycleId
+			})
+
+			if (currentCycleIndex < 0) {
+				return state
 			}
 
-		case ActionTypes.INTERRUPT_CURRENT_CYCLE:
-			return {
-				...state,
-				cycles: state.cycles.map((cycle) => {
-					return cycle.id === state.activeCycleId
-						? { ...cycle, interruptedDate: new Date() }
-						: cycle
-				}),
-				activeCycleId: null,
+			return produce(state, (draft) => {
+				draft.cycles[currentCycleIndex].interruptedDate = new Date()
+				draft.activeCycleId = null
+			})
+		}
+		case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
+			const currentCycleIndex = state.cycles.findIndex((cycle) => {
+				return cycle.id === state.activeCycleId
+			})
+
+			if (currentCycleIndex < 0) {
+				return state
 			}
 
-		case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-			return {
-				...state,
-				cycles: state.cycles.map((cycle) => {
-					return cycle.id === state.activeCycleId
-						? { ...cycle, finishedDate: new Date() }
-						: cycle
-				}),
-			}
+			return produce(state, (draft) => {
+				draft.cycles[currentCycleIndex].finishedDate = new Date()
+				draft.activeCycleId = null
+			})
+		}
+
 		default:
 			return state
 	}
